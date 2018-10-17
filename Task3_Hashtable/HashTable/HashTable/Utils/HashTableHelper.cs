fa0e3f05b8d0f905ce25a9d816068eb0e78206b2
IvanShinkevich <2014ivanee@gmail.com>
2018-10-17 23:53:20 +0300

    added base hashtable with chains

---
+++ Task3_Hashtable/HashTable/HashTable/Utils/HashTableHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HashTable.Entities;
+
+namespace HashTable.Utils
+{
+    public static class HashTableHelper
+    {
+        /// <summary>
+        /// Вывод хеш-таблицы на экран.
+        /// </summary>
+        /// <param name="hashTable"> Хеш таблица. </param>
+        /// <param name="title"> Заголовок перед выводом таблицы. </param>
+        public static void ShowHashTable(Entities.HashTableChains hashTable, string title)
+        {
+            // Проверяем входные аргументы.
+            if (hashTable == null)
+            {
+                throw new ArgumentNullException(nameof(hashTable));
+            }
+
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            // Выводим все имеющие пары хеш-значение
+            Console.WriteLine(title);
+            foreach (var item in hashTable.Items)
+            {
+                // Выводим хеш
+                Console.WriteLine(item.Key);
+
+                // Выводим все значения хранимые под этим хешем.
+                foreach (var value in item.Value)
+                {
+                    Console.WriteLine($"\t{value.Key} - {value.Value}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
